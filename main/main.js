a39d031388a7ab8f829c751efbb9486b697ad9d3
yaoxiaoyu <yaoxiao0525@foxmail.com>
2018-05-05 19:53:39 +0800

    finished guess-number

--- main/main.js
+++ main/main.js
@@ -1,6 +1,57 @@
 // Write your cade below:
-function main() {};
+// 由于随机数无法预测，不便于测试，所以生成随机数到函数先注释掉，用测试用例中的输入代替，等到用例通过后再测试随机数也能通过测试。
+// function getRandomString(){
+//     var randomString = Math.floor(Math.random() * 10) + '';
+//     for(var i = 0; i < 3; i ++){
+//         newRandomNum = Math.floor(Math.random() * 10); 
+//         for(var j in randomString){
+//             while(newRandomNum === randomString.charAt(j)){
+//                 newRandomNum = Math.floor(Math.random() * 10);
+//             }     
+//         } 
+//         randomString = randomString + newRandomNum;
+//     }
+//     return randomString;
+// }
 
-module.exports = {
-    main
-}
+function main(input) {
+    var result = '';
+    // var randomString = getRandomString();
+    var randomString = '5678';
+    var count_a = 0;
+    var count_b = 0;
+
+    //判断输入中是否有重复值
+    var flag = true;
+    for(var i = 0; i < input.length; i ++){
+        if(input.charAt(i) === input.charAt(i + 1)){
+            flag = false;
+            break;
+        };
+    }
+
+    ///判断输入中是否有重复值，若没有才比较输入和随机值；若有，提示‘请输入4个不同的数字’
+    if(flag === true ){
+        for(var i = 0; i < 4; i ++){
+            if(input.charAt(i) === randomString.charAt(i)){
+                count_a ++;
+            }else{
+                for(var j = 0; j < 4; j ++){
+                    if(input.charAt(i) === randomString.charAt(j)){
+                        count_b ++;
+                        break;
+                    }
+                }
+            }
+        }
+        result = count_a + 'A' + count_b + 'B';
+        return result;
+    }else{
+        return '请输入4个不同的数字';
+    }
+    
+};
+
+module.exports = main
+// var input = '5667';
+// main(input)
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,34 @@
 const main = require('../main/main');
 
-describe('main()', () => {
+describe('main()', function() {
+    var result1 = '0A0B';
+    var result2 = '0A2B'
+    var result3 = '1A1B'
+    var result4 = '4A0B'
+    it('全部没猜中', function(){
+        var expectResult = main('1234');
+        expect(expectResult).toEqual(result1);
+    });
+
+    it('猜中数字，但没猜中位置', function(){
+        var expectResult = main('1256');
+        expect(expectResult).toEqual(result2);
+    });
+
+    it('猜中1个数字和它的位置，猜中1个数字但是没猜中位置', function(){
+        var expectResult = main('1652');
+        expect(expectResult).toEqual(result3);
+    });
+
+    it('猜中全部数字和它的位置', function(){
+        var expectResult = main('5678');
+        expect(expectResult).toEqual(result4);
+    });
+
+    it('输入相同的数字', function(){
+        var expectResult = main('5668');
+        expect(expectResult).toEqual('请输入4个不同的数字');
+    });
+
 
 });
