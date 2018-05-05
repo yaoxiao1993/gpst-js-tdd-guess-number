62a32f8c766955f37829ea2acff384a5ee15b182
yaoxiaoyu <yaoxiao0525@foxmail.com>
2018-05-06 00:48:35 +0800

    修复两个bug，一个是随机数中有重复数字，一个是输入中不连续的数字相同时无法判断为输入中存在重复数字
    q
    exit
    exit()

--- main/main.js
+++ main/main.js
@@ -1,33 +1,48 @@
 // Write your cade below:
 // 由于随机数无法预测，不便于测试，所以生成随机数到函数先注释掉，用测试用例中的输入代替，等到用例通过后再测试随机数也能通过测试。
-// function getRandomString(){
-//     var randomString = Math.floor(Math.random() * 10) + '';
-//     for(var i = 0; i < 3; i ++){
-//         newRandomNum = Math.floor(Math.random() * 10); 
-//         for(var j in randomString){
-//             while(newRandomNum === randomString.charAt(j)){
-//                 newRandomNum = Math.floor(Math.random() * 10);
-//             }     
-//         } 
-//         randomString = randomString + newRandomNum;
-//     }
-//     return randomString;
-// }
+//生成不重复的4个随机数组成的字符串
+function getRandomString(){
+    var randomString = Math.floor(Math.random() * 10) + '';
+    var isSameNum;
+    var newRandomNum;
+    for(var i = 0; i < 3; i ++){
+        newRandomNum = Math.floor(Math.random() * 10); 
+        do{
+            for(var j in randomString){
+                if(newRandomNum === parseInt(randomString.charAt(j))){
+                    newRandomNum = Math.floor(Math.random() * 10);
+                    isSameNum = true;
+                    break;
+                }else{
+                    isSameNum = false;
+                }  
+            }
+        }while(isSameNum === true);
+        randomString = randomString + newRandomNum;
+    }
+    console.log('randoem string is: '+randomString)
+    return randomString;
+}
 
 function main(input) {
     var result = '';
-    // var randomString = getRandomString();
+    //var randomString = getRandomString();
     var randomString = '5678';
     var count_a = 0;
     var count_b = 0;
 
     //判断输入中是否有重复值
     var flag = true;
-    for(var i = 0; i < input.length; i ++){
-        if(input.charAt(i) === input.charAt(i + 1)){
+    var arr = [];
+    for(var i in input){
+        arr.push(input.charAt(i))
+    };
+    arr.sort();
+    for(var i = 0; i < arr.length; i ++){
+        if(arr[i] === arr[i+1]){
             flag = false;
             break;
-        };
+        }
     }
 
     ///判断输入中是否有重复值，若没有才比较输入和随机值；若有，提示‘请输入4个不同的数字’
